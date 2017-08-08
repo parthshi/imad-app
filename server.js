26565c2bd40d1b5a3a0ad325e632ebe6e42048aa
parthshi <30660354+parthshi@users.noreply.github.com>
2017-08-08 21:05:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/artical-one',function(req,res){
     
-    res.sendFile(path.join(_dirname,'ui','artical-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
