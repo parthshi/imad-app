d29a8c43e643d3400cc161a5f9a8938651c61161
parthshi <30660354+parthshi@users.noreply.github.com>
2017-08-12 23:31:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,27 @@
-console.log('Loaded!');
+// counter code
+var button  = document.getElementById('counter');
+
+
+button.onclick = function () {
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture response and store it in a variable
+    request.onreadystatechange = function(){
+        
+        if(request.readystate == XMLHttpRequest.DONE){
+            //take some action
+            if(request.status ==200){
+                var counter = request.responseText;
+                var span = document.getElementById('counter');
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+        //Not done yet
+    };
+    //make the requet
+    request.open('GET','http://parthshinde71994.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
